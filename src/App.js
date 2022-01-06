d87de718a2a0cd7c73432298c44dcb491c5760b2
kiiramat <kiiramati@gmail.com>
2022-01-06 17:41:55 +0000

    made modifications to logo. Styled page. Added github icon and created link.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import logo from "./images/logo.png"
+import GithubIcon from "./images/svgs/GithubIcon"
+import './App.css';
 
 export default function App() {
   return (
     <div>
-      <header>
-        This is the top
+      <header className="header">
+        <img className="logo" src={logo} alt="Logo" />
+        <nav className="navbar">
+          <Link to="/">Projects</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="https://github.com/kiiramat"><GithubIcon className="icon-link" /></Link>
+        </nav>
       </header>
+      
       <Outlet />
       <footer>
         This is the bottom
