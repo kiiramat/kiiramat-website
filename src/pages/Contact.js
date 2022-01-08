74d053e74228577ced59535b17bdeafe7bc84017
kiiramat <kiiramati@gmail.com>
2022-01-08 15:04:20 +0000

    created 2 columns in Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,23 @@
-import envelope from "../images/envelope.png"
-import "./Contact.css"
+import envelope from "../images/envelope.png";
+import "./Contact.css";
 
 function Contact() {
-    return (
-        <div className="contact-div">
-            <img className="envelope" src={envelope} alt="Envelope" />
+  return (
+    <div className="contact-container">
+      <div className="contact-div">
+        <div className="horizontal">
+          <div className="vertical"> 
+            <div className="envelope-div">
+              <img className="envelope" src={envelope} alt="Envelope" />
+            </div>
+          </div>
+          <div className="vertical">
+            
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
