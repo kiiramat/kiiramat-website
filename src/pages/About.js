01fbc757d86ab174e54e3c936a6ca0c735a9614d
kiiramat <kiiramati@gmail.com>
2022-01-11 16:42:45 +0000

    About page implemented

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,19 @@
-import turtle from "../images/turtle300.png"
-import "./About.css"
+import turtle from "../images/turtle300.png";
+import "./About.css";
 
 function About() {
-    return (
-        <div className="about-div">
-            <img className="turtle" src={turtle} alt="Turtle" />
-        </div>
-    );
+  return (
+    <div className="about-container">
+      <div className="vertical about-div">
+        <img className="turtle-portrait" src={turtle} alt="Turtle" />
+        <p className="about-me-text">
+          I'm an ex-health care provider that lives with a Software Developer.<br /><br />
+          Since I used to be interested in maths, my other half's job got my attention.<br /><br />
+          With a mentor at home and by following some tutorials, I have done projects in JavaScript, HTML, CSS and React.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
