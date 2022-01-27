32c2289d13196c5e55746de9c7802739d4bfe940
kiiramat <kiiramati@gmail.com>
2022-01-27 21:01:16 +0000

    turtle layout implemented

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,12 @@
 import React from "react";
 import TurtleLayout from "../components/TurtleLayout";
+import './Projects.css'
 
 function Projects() {
     return (
-        <div>
-            <TurtleLayout />
+        <div className="turtle-layout-container">
+            <TurtleLayout title="Project 1" text="Hello this is a project that I did" />
+            <TurtleLayout title="Project 1" text="Hello this is a project that I did" />
         </div>
     );
 }
