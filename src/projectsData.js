fca38f19ccc24595703b9221a00ea21dd32d271e
kiiramat <kiiramati@gmail.com>
2022-02-04 16:31:05 +0000

    created file with projects data

---
+++ src/projectsData.js
@@ -0,0 +1,48 @@
+const projectData = [
+    {
+        id: 1,
+        title="Timer",
+        text="I wanted an alarm and a timer where I could customize the alarm tone (with youtube or mp3 links)",
+        githubHref="https://github.com/kiiramat/timer",
+        linkHref="https://kiiramat.com/timer/"
+    },
+    {
+        id: 2,
+        title="HALF",
+        text="Everybody has annoying (loving) family members who you only want to contact on their birthday and Christmas. I created HALF (Handling Annoying Loving Family) so that I could automate the messages sent to these family members ",
+        githubHref="https://github.com/kiiramat/HALF"
+    },
+    {
+        id: 3,
+        title="Hangman",
+        text="I used to play hangman as a kid, so I thought I could do it in JavaScript",
+        githubHref="https://github.com/kiiramat/hangman",
+        linkHref="https://kiiramat.com/hangman/"
+    },
+    {
+        id: 4,
+        title="Todo List",
+        text="I needed a todo list so I implemented one. This was implemented in plain JavaScript so that I could then implement it in React to better understand the differences",
+        githubHref="https://github.com/kiiramat/todoList-vanilla-js",
+        linkHref="https://kiiramat.com/todoList-vanilla-js/"
+    },
+    {
+        id: 5,
+        title="Travel Form",
+        text="Implemented while following a Scrimba tutorial to learn React",
+        githubHref="https://github.com/kiiramat/travelForm-react"
+    },
+    {
+        id: 6,
+        title="Todo List",
+        text="I created this todo list in React because I wanted to reimplement a previous JavaScript project in React",
+        githubHref="https://github.com/kiiramat/todoList-react",
+        linkHref="https://kiiramat.com/todo/"
+    },
+    {
+        id: 7,
+        title="Website",
+        text="You are on it :D",
+        githubHref="https://github.com/kiiramat/kiiramat-website"
+    }
+]
