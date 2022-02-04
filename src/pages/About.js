59f393a5f0ecd81a33b5afebfcf06ab384160731
kiiramat <kiiramati@gmail.com>
2022-02-04 13:06:37 +0000

    Added envelope and portrait images. Put portrait in about page and styled it

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,16 @@
-import turtle from "../images/turtle300.png";
+import TurtlePortrait from "../components/TurtlePortrait";
 import "./About.css";
 
 function About() {
   return (
     <div className="about-container">
-      <div className="vertical about-div">
-        <img className="turtle-portrait" src={turtle} alt="Turtle" />
+      <div className="horizontal about-div">
         <p className="about-me-text">
           I'm an ex-health care provider that lives with a Software Developer.<br /><br />
           Since I used to be interested in maths, my other half's job got my attention.<br /><br />
           With a mentor at home and by following some tutorials, I have done projects in JavaScript, HTML, CSS and React.
         </p>
+        <TurtlePortrait />
       </div>
     </div>
   );
